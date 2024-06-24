0247c70381bc1f4a7f929590643f5e727c8eb45e
BRAGA <65302648+bragadz@users.noreply.github.com>
2024-06-24 18:59:58 -0300

    Delete NeverMiss Home directory

--- NeverMiss Home/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const cursorDot = document.querySelector('.cursor-dot');
-    const cursorOutline = document.querySelector('.cursor-outline');
-
-    cursorDot.style.left = e.clientX + 'px';
-    cursorDot.style.top = e.clientY + 'px';
-    cursorOutline.style.left = e.clientX + 'px';
-    cursorOutline.style.top = e.clientY + 'px';
-});
